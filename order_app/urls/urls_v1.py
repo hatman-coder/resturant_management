792edbf371d02f506807487fd49b3725f64d38d7
Masud Bhuiyan <masud.bhuiya.m2@gmail.com>
2024-08-25 14:05:09 +0600

    url change

--- order_app/urls/urls_v1.py
+++ order_app/urls/urls_v1.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('order-status-update/<str:id>/', OrderViewSet.as_view({'put': 'order_status_update'})),
     path('list/', OrderViewSet.as_view({'get': 'list'})),
     path('retrieve-order/<str:id>/', OrderViewSet.as_view({'get': 'retrieve_order'})),
-    path('track-order/', OrderViewSet.as_view({'delete': 'trak_order'}))
+    path('track-order/<str:traking_id>/', OrderViewSet.as_view({'delete': 'trak_order'}))
 ]
